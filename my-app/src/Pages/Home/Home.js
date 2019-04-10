f09babfc6f869264af33a424ea549341c91933cd
Kaleigh Herold <kaleigh224@gmail.com>
2019-04-10 13:57:07 -0400

    added home and reviews pages

---
+++ my-app/src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class HomePage extends Component {
+    render () {
+        return (
+            <div className='home'>
+            </div>
+        )
+    }
+}
+
+export default HomePage;
---
+++ my-app/src/Pages/Reviews/Reviews.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '.reviews.css';
+
+class ReviewsPage extends Component {
+    render () {
+        return (
+            <div className='reviews'></div>
+        )
+    }
+}
+
+export default ReviewsPage;
