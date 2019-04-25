1871a08091763d9f111e67468ee9cda0ee84db6d
Kaleigh Herold <kaleigh224@gmail.com>
2019-04-25 13:50:18 -0400

    added photo of Kyson

--- my-app/src/Pages/Home/Home.js
+++ my-app/src/Pages/Home/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './Home.css';
+import Kyson from '../../images/KysonBW.jpeg'
 
 class HomePage extends Component {
     render () {
         return (
             <div className='home'>
+            <nav><h1>This is a NavBar</h1></nav>
+            <img src={Kyson} className="Kyson" alt="Dog on table"/>
             </div>
         )
     }
